50eee19fa9fd2aae3fb79d7fcd4420a1eefa070f
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-09-20 20:59:25 -0700

    final tweaks and bug fixes

--- server.js
+++ server.js
@@ -5,24 +5,19 @@ var path = require("path");
 //Set Up Express
 var app = express();
 var PORT = process.env.PORT || 8000;
-/*
-var bodyParser = require("body-parser");
 
-//Set up parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-*/
 //Route Files
 var apiRoutes = require("./app/routing/apiRoutes.js");
 var htmlRoutes = require("./app/routing/htmlRoutes.js");
 var router = express.Router();
-app.use('/', router); // Mount the router as middleware at path /
-app.use(express.static(path.join(__dirname, '/app/public')));
+app.use('/', router); // Mount the router as middleware at path 
+//Route to apiRoutes file for API routes
 router.all('/api', apiRoutes);
 router.post('/api/:new', apiRoutes);
-router.get('/', htmlRoutes); //for Home page
+//Route to htmlRoutes file for HTML routes
+router.all('/', htmlRoutes); 
+router.get('/survey', htmlRoutes);
+router.get('/style', htmlRoutes);
 
 //Set up Listening
 app.listen(PORT, function(){
